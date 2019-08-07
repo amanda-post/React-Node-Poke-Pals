b8334db5e45bac02ae843ee674d3cdee573e03d2
Amanda Post <amanda.post44@gmail.com>
2019-08-06 18:42:58 -0700

    [feat] users can delete messages only from their personal inbox. friend request functionality built out, starting testing.

--- server/controller.js
+++ server/controller.js
@@ -1,8 +1,7 @@
 const { User, Message, Pokemon } = require('../database/index.js');
 const mongoose = require('mongoose');
-const moment = require('moment');
 
-const fn = {
+const controlller = {
   // Checks if username exists; if not, creates a new database entry for that username.
   handleNewUser: (req, res) => {
     let { username } = req.body;
@@ -21,24 +20,24 @@ const fn = {
   
   // Finds all data for user's profile (friends list, pokemon list, etc.)
   getUser: (req, res) => {
-    let username = req.params; // check this
+    let { uid } = req.params;
     User
-    .find({username: username})
-    .then((data) => {
-      console.log(`Successfully got data for user: ${username}`);
-      res.status(200).send(data);
-    })
-    .catch((err) => {
-      console.log('Error with getUser:', err);
-      res.status(404).send(`Error loading profile for user: ${username}`);
-    });
+      .findOne({uid: uid})
+      .then((data) => {
+        console.log(`Successfully got data for user: ${uid}`);
+        res.status(200).send(data);
+      })
+      .catch((err) => {
+        console.log('Error with getUser:', err);
+        res.status(404).send(`Error loading profile for user: ${uid}`);
+      });
   },
   
   // Finds all messages sent or received by the requesting user.
   getMessagesForUser: (req, res) => {
     let { username } = req.params;
     Message
-      .find({ $or: [{sender: username}, {receiver: username}] }).limit(15).sort({timeStamp: 1})
+      .find({username: username}, 'messages').limit(15).sort({timeStamp: -1})
       .then((data) => {
         console.log(`Successfully got messages for username: ${username}`);
         res.status(200).send(data);
@@ -75,60 +74,81 @@ const fn = {
   handleSentMessage: (req, res) => {
     let { sender, receiver, content } = req.body;
     Message
-      .create({sender, receiver, content, timeStamp: moment(new Date()).format('lll')})
-      .then((docs) => {
-        console.log(`Successfully handled sent message from ${sender} to ${receiver}`);
-        res.status(202).send(`Successfully sent message to ${receiver}!`);
+      .update([{username: sender}, {$push: {messages: {sender, receiver, content, timeStamp: new Date()}}},
+        {username: receiver}, {$push: {messages: {sender, receiver, content, timeStamp: new Date()}}}])
+      .then((doc) => {
+        res.status(201).send();
+        console.log(`Successfully handled sent message from ${sender} to ${receiver}.`, doc);
       })
       .catch((err) => {
-        console.log(`Error handling sent message from ${sender} to ${receiver}`, err);
-        res.status(404).send(`Error sending message to ${receiver}!`);
+        res.status(404).send(err);
+        console.log(`Error handling sent message from ${sender} to ${receiver}.`, err);
       });
   },
   
   // Removes a message by id from a user's messages
   handleDeleteMessage: (req, res) => {
-    let { messageId } = req.params;
-    console.log('dis the id', messageId)
+    let { username, messageId } = req.params;
+    console.log('req.body-----------------------------', req.body)
     Message
-      .findOneAndRemove({_id: messageId})
+      .update({username: username}, {$pull: {messages: {_id: messageId}}})
       .then((doc) => {
-        res.status(200).send(doc)
+        res.status(204).send();
+        console.log(`${username}'s message id: ${messageId} deleted.`, doc);
       })
       .catch((err) => {
-        res.status(404).send(err)
+        res.status(404).send(err);
+        console.log(`Couldn't delete ${username}'s message id: ${messageId}.`, err);
       });
   },
 
   // Puts friend request in receiver's pending requests in database.
-  handleSentRequest: (req, res) => {
+  handleSentFriendRequest: (req, res) => {
     let { sender, receiver } = req.body;
-    database
-      .handleSentRequest(sender, receiver)
-      .then((docs) => {
-        console.log('Successfully handled sent request for users:', sender, receiver);
-        res.status(202).send(`Successfully sent friend request to ${receiver}!`);
+    User
+      .update({username: receiver}, {$push: {friendRequests: sender}})
+      .then((doc) => {
+        res.status(202).send();
+        console.log(`Friend request sent from ${sender} to ${receiver}.`, doc)
       })
       .catch((err) => {
-        console.log('Error handling sent friend request for users:', sender, receiver, err);
-        res.status(404).send(`Error sending friend request to ${receiver}!`);
+        res.status(404).send(err);
+        console.log(`Error sending friend request from ${sender} to ${receiver}.`, err);
       });
   },
 
-  // Adds sender to receiver's friends list in database, and vice versa.
-  handleAcceptedRequest: (req, res) => {
-    let { sender, receiver } = req.body;
-    database
-      .handleAcceptedRequest(sender, receiver)
-      .then((docs) => {
-        console.log('Successfully handled accepted request for users:', sender, receiver);
-        res.status(202).send(`Successfully accepted friend request from ${sender}!`);
+  // If accepted, adds sender to receiver's friends list in database, and vice versa.
+  // If declined, removes the pending request from the receiver's list
+  handleResponseToFriendRequest: (req, res) => {
+    let { sender, receiver, response } = req.body;
+    if (response === "accept") {
+      User
+        .update({username: sender}, {$push: {friendsList: receiver}})
+        .then(User.update({username: receiver}, {$push: {friendsList: sender}}))
+        .then((doc) => {
+          res.status(201).send();
+          console.log(`Successfully handled accepted request from ${sender} to ${receiver}`, doc);
+        })
+        .catch((err) => {
+          res.status(404).send(err);
+          console.log('Error handling accepted friend request for users:', sender, receiver, err);
+        });
+    } 
+    User
+      .update({username: receiver}, {$pull: {friendRequests: sender}})
+      .then((doc) => {
+        res.status(204).send();
+        console.log(`Successfully removed friend request from ${sender} to ${receiver}.`, doc);
       })
       .catch((err) => {
-        console.log('Error handling accepted friend request for users:', sender, receiver, err);
-        res.status(404).send(`Error sending accepting friend request from ${sender}!`);
+        res.status(404).send(err);
+        console.log(`Error with friend request from ${sender} to ${receiver}.`, err);
       });
-  }
+  },
+
+  handleDeleteFriend: (req, res) => {
+
+  },
 }
 
-module.exports = fn;
+module.exports = controlller;
