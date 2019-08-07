32a3823308e3173649d8b16b1dbfac0ca2aa3645
Amanda Post <amanda.post44@gmail.com>
2019-08-06 18:44:00 -0700

    App.jsx makes a call to get user data upon page load; currently using placeholder data until firebase auth is completely set up.

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -1,3 +1,75 @@
-import React from 'react';
+import React from "react";
+import axios from "axios";
+// import firebase from '../../../init-firebase.js';
+
+import Login from "./Login.jsx";
+import Friends from "./Friends/Friends.jsx";
+import Messages from "./Messages/Messages.jsx";
+import Pokemon from "./Pokemon.jsx";
+
+// var user_logged_in = firebase.auth().currentUser;
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      username: "amanda_toast", //todo
+      friendsList: [],
+      friendRequests: []
+    };
+    this.getUserInfo = this.getUserInfo.bind(this);
+  }
+
+  componentWillMount() {
+    this.getUserInfo();
+  }
+
+  getUserInfo() {
+    axios
+      .get('api/users/295967108669') // REPLACE WITH <user_logged_in> (firebase uid)
+      .then(({ data }) => {
+        let { username, friendsList, friendRequests } = data;
+        this.setState({
+          username,
+          friendsList,
+          friendRequests
+        });
+      })
+      .catch(err => {
+        console.log("Error getting user data", err);
+      });
+  }
+
+  render() {
+    // if (!user) {
+    //   return (
+    //   <div>
+    //     <Login />
+    //   </div>
+    //   )
+    // } else {
+    //   return (
+    //     <div>
+    //       <Friends />
+    //       <Messages />
+    //       <Pokemon />
+    //     </div>
+    //   )
+    // }
+    return (
+      <div>
+        <Login />
+        <Friends
+          username={this.state.username}
+          friendsList={this.state.friendsList}
+          friendRequests={this.state.friendRequests}
+          update={this.getUserInfo}
+        />
+        <Messages username={this.state.username} />
+        <Pokemon />
+      </div>
+    );
+  }
+}
 
 export default App;
