7468190185749545c83e3fcb85fad685b970276b
Amanda Post <amanda.post44@gmail.com>
2019-08-07 13:58:02 -0700

    each friend component has a delete button, which will remove the two users from each others friend lists in the database. it will automatically re-load data to reflect the deletion. reply button functionality to be built out.

--- client/src/components/Friends/FriendList.jsx
+++ client/src/components/Friends/FriendList.jsx
@@ -1,5 +1,14 @@
-import React from 'react';
+import React from "react";
+import FriendsListEntry from "./FriendListEntry.jsx";
 
-
+const FriendList = (props) => {
+  return (
+    <div>
+      {props.friendsList.map((friend, i) => (
+        <FriendsListEntry friend={friend} username={props.username} update={props.update} key={i} />
+      ))}
+    </div>
+  );
+};
 
 export default FriendList;
--- client/src/components/Friends/FriendListEntry.jsx
+++ client/src/components/Friends/FriendListEntry.jsx
@@ -1,4 +1,29 @@
-import React from 'react';
+import React from "react";
+import axios from "axios";
 
+const FriendListEntry = (props) => {
+  const deleteFriend = () => {
+    let users = { 
+      user1: props.username, 
+      user2: props.friend 
+    }
+    axios
+      .delete("/api/friends", { data: users})
+      .then(() => {
+        props.update();
+      })
+      .catch((err) => {
+        console.log("Error deleting friend", err);
+      });
+  };
+
+  return (
+    <div>
+      {props.friend}
+      <button onClick={deleteFriend}>Unfriend</button>
+      <button>Send a message</button>
+    </div>
+  );
+};
 
 export default FriendListEntry;
